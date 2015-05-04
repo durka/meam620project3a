b4245a14308b83633daf5d4ad142970b8d8bf01a
Alex Burka <aburka@seas.upenn.edu>
2015-05-04 14:40:41 -0400

    plot paths and make movies for DCAPT2D

--- dcapt2d.m
+++ dcapt2d.m
@@ -1,18 +1,11 @@
-function dcapt2D(starts, goals, R, H, tf, dt, plotting)
+function dcapt2d(starts, goals, R, H, tf, dt, plotting)
 
 close all
+if nargin < 7
+    plotting = false;
+end
 
 [starts,goals] = shift_points(starts,goals,R);
-
-traj.starts = starts;
-traj.goals = goals;
-traj.R = R;
-traj.H = H;
-traj.current = starts;
-n_bots = size(starts,1);
-
-working = true;
-
 
 
 N = size(starts, 1);
@@ -26,14 +19,29 @@ if size(goals, 2) ~= n
     error('Different dimensions in start and goal space!');
 end
 
+traj.starts = starts;
+traj.goals = goals;
+traj.R = R;
+traj.H = H;
+traj.current = starts;
+traj.paths = cell(1,N);
+for i=1:N
+    traj.paths{i} = [starts(i,:); goals(i,:)];
+end
+
 v = (goals - starts)/tf; 
+
+if ischar(plotting)
+    writer = VideoWriter(plotting);
+    open(writer);
+end
 
 current = starts;
 for tc = 0:dt:(tf-dt)
     dists = squareform(pdist(current));
     % Do DCAPT
-    for j = 1:n_bots
-      for i = 1:n_bots
+    for j = 1:N
+      for i = 1:N
         if i == j
           continue
         end
@@ -47,6 +55,12 @@ for tc = 0:dt:(tf-dt)
 
             v(i,:) = (goals(i,:) - current(i,:))/(tf - tc);
             v(j,:) = (goals(j,:) - current(j,:))/(tf - tc);
+            
+            traj.paths{i}(end,:) = current(i,:);
+            traj.paths{i}(end+1,:) = goals(i,:);
+            traj.paths{j}(end,:) = current(j,:);
+            traj.paths{j}(end+1,:) = goals(j,:);
+            
             fprintf('switching %d and %d \n',i,j)
           end
           
@@ -55,10 +69,18 @@ for tc = 0:dt:(tf-dt)
     end
     current = current + v*dt;
     traj.current = current;
-    plot_dcapt(traj)
+    
+    if plotting
+        plot_dcapt(traj);
+        if ischar(plotting)
+            writeVideo(writer, getframe(gcf));
+        end
+    end
 end
  
- 
+if ischar(plotting)
+    close(writer);
+end
       
     
     % Break when near goals
--- make_movies.m
+++ make_movies.m
@@ -6,3 +6,7 @@ plot_ccapt(ccapt([1 1; 2 2], [2 1; 1 2], 0.5, 1, 0.01), 0.5, 0.01, 'movies/ccapt
 
 record = 'movies/ccaptQuad_random_waypts.avi';
 runsim;
+
+%% phase 3
+
+dcapt2d(rand(10, 2)*50, rand(10, 2)*100, 0.5, 10, 10, 0.1, 'movies/dcapt2D.avi');
