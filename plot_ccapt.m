825c05792c697bf8afc2ccacae88c781092b3b72
Alex Burka <aburka@seas.upenn.edu>
2015-05-04 10:48:17 -0400

    video recording for 2D plot

--- plot_ccapt.m
+++ plot_ccapt.m
@@ -1,4 +1,9 @@
-function plot_ccapt(T, R, dt)
+function plot_ccapt(T, R, dt, record)
+
+if nargin == 4
+    writer = VideoWriter(record);
+    open(writer);
+end
 
 n_T = size(T,1)/2;
 starts = reshape(T(:,1),2,n_T)';
@@ -13,9 +18,27 @@ scatter(goals(:,1), goals(:,2),300, 'p', 'markerfacecolor', 'b', 'markeredgecolo
 
 circ = @(t) viscircles(reshape(T(:,t), [2 n_T])', ones(n_T, 1)*R, 'edgecolor','g');
 step = circ(1);
+legend(get(gca, 'Children'), 'Robot', 'Start', 'Goal');
+xlabel('X (m)');
+ylabel('Y (m)');
+title('Frame 1 (0 s)');
+
+if exist('writer', 'var')
+    writeVideo(writer, getframe(gcf));
+end
 
 for i = 2:size(T,2)
+    tic;
     delete(step);
     step = circ(i);
-    drawnow;pause(dt);
+    title(sprintf('Frame %d (%g s)', i, i*dt));
+    drawnow;
+    if exist('writer', 'var')
+        writeVideo(writer, getframe(gcf));
+    end
+    pause(dt - toc);
+end
+
+if exist('writer', 'var')
+    close(writer);
 end
