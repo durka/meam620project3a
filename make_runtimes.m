1ee8e89bb0ee04d435afe6d7be5e7eccb6f6183b
Alex Burka <aburka@seas.upenn.edu>
2015-05-04 14:03:56 -0400

    timing loop

---
+++ make_runtimes.m
@@ -0,0 +1,36 @@
+%% phase 1
+
+N = [10 50 100:100:1000];
+n = 10;
+R = 0.5;
+vmax = 1;
+dt = 0.01;
+
+timings = zeros(size(N), n);
+gamma = cell(size(N));
+starts = cell(size(N));
+goals = cell(size(N));
+for i=1:length(N)
+    fprintf('Timing CCAPT-2D for %d robots...\n', N(i));
+    
+    % move the starts and goals
+    [gamma{i}, starts{i}, goals{i}] = ccapt(rand(N(i), 2)*50, rand(N(i), 2)*100, R, vmax, dt);
+    
+    % average timings
+    for j=1:n
+        fprintf('\t%d\n', j);
+        tic;
+        ccapt(starts{i}, goals{i}, R, vmax, dt);
+        timings(i,j) = toc;
+    end
+end
+
+loglog(N, mean(timings, 2));
+grid on;
+xlabel('Number of robots');
+ylabel('Planning time (s)');
+title('C-CAPT 2D Planning performance');
+print -depsc timing_phase1
+
+save timing_phase1 N n R vmax dt gamma starts goals timings
+
