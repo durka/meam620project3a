87df60868d75b96775d4ae73714c40e026731c6c
Alex Burka <aburka@seas.upenn.edu>
2015-05-04 11:16:49 -0400

    fix 2D plot legends

--- plot_ccapt.m
+++ plot_ccapt.m
@@ -13,12 +13,12 @@ range = [min([starts; goals],[],1)-2, max([starts; goals],[],1)+2];
 figure(1);clf;axis([range(1) range(3) range(2) range(4)]);axis equal;
 hold on; grid on; grid minor;
 plot(T(1:2:end,:)', T(2:2:end,:)','b'); % plot full trajectories
-scatter(starts(:,1), starts(:,2),300, 's', 'markerfacecolor', 'r', 'markeredgecolor', 'k');
-scatter(goals(:,1), goals(:,2),300, 'p', 'markerfacecolor', 'b', 'markeredgecolor', 'k');
+hs = scatter(starts(:,1), starts(:,2),300, 's', 'markerfacecolor', 'r', 'markeredgecolor', 'k');
+hg = scatter(goals(:,1), goals(:,2),300, 'p', 'markerfacecolor', 'b', 'markeredgecolor', 'k');
 
 circ = @(t) viscircles(reshape(T(:,t), [2 n_T])', ones(n_T, 1)*R, 'edgecolor','g');
 step = circ(1);
-legend(get(gca, 'Children'), 'Robot', 'Start', 'Goal');
+legend([hs, hg], 'Start', 'Goal');
 xlabel('X (m)');
 ylabel('Y (m)');
 title('Frame 1 (0 s)');
